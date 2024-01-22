4212a704ef12f1651cb8164684ede921f1da034f
Youcef <youcef.boulfrad@yahoo.fr>
2024-01-22 16:09:17 +0100

    python

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 
 import pytz # $ pip install pytz
@@ -11,3 +12,14 @@ current_time_formatted = current_time.strftime("%H:%M:%S")
 
 print(current_time_formatted)
 
+logging.basicConfig(filename=None, encoding='utf-8', level=logging.DEBUG)
+
+logging.info(f"Lancement du traitement")
+
+logging.debug(f"Demande d'heure sur le timezone : {timezone_paris}")
+
+if timezone_paris is None:
+
+    logging.error("aucune timezone n'a été renseignée")
+
+    raise ValueError("aucune timezone n'a été renseignée")
---
+++ readme.md
@@ -0,0 +1,6 @@
+# TP conception logicielle ENSAI
+
+télécharger les dépendances : 'pip install -r requirements.txt'
+
+lancer le code 'python main.py'
+
